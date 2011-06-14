40d175ec8924cf747367509e4e459a852623b088
Philip Dodds <philip.dodds@me.com>
2011-06-14 18:05:28 -0400

    first commit

---
+++ src/main/java/com/fiveclouds/jasper/JasperRunner.java
@@ -0,0 +1,65 @@
+package com.fiveclouds.jasper;
+
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import org.apache.commons.cli.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * A simple helper which can be used to export a PDF of a Jasper Report
+ * <p/>
+ * Note this is a very simple little utility
+ */
+public class JasperRunner {
+
+    public void main(String[] args) {
+
+        // Set-up the options for the utility
+        Options options = new Options();
+        options.addOption("report", true, "jasper report to run (i.e. /path/to/report.jrxml)");
+        options.addOption("driver", true, "the jdbc driver class (i.e. com.mysql.jdbc.Driver)");
+        options.addOption("jdbcUrl", true, "database jdbc url (i.e. jdbc:mysql://localhost:3306/database)");
+        options.addOption("username", true, "database username");
+        options.addOption("password", true, "database password");
+        options.addOption("output", true, "the output filename (i.e. path/to/report.pdf");
+
+        Option property = OptionBuilder.withArgName("property=value")
+                .hasArgs(2)
+                .withValueSeparator()
+                .withDescription("use value for given property")
+                .create("D");
+
+        options.addOption(property);
+
+        // Parse the options and build the report
+        CommandLineParser parser = new PosixParser();
+        try {
+            CommandLine cmd = parser.parse(options, args);
+            try {
+                Class.forName(cmd.getOptionValue("driver"));
+                Connection connection = DriverManager.getConnection(cmd.getOptionValue("jdbcUrl"), cmd.getOptionValue("username"), cmd.getOptionValue("password"));
+                JasperReport jasperReport = JasperCompileManager.compileReport(cmd.getOptionValue("report"));
+                JRPdfExporter pdfExporter = new JRPdfExporter();
+                JasperPrint print = JasperFillManager.fillReport(jasperReport, cmd.getOptionProperties("D"), connection);
+
+                pdfExporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
+                pdfExporter.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, cmd.getOptionValue("output"));
+                pdfExporter.exportReport();
+            } catch (JRException e) {
+                System.err.print("Unable to parse report file (" + cmd.getOptionValue("r") + ")");
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                System.err.print("Unable to find the database driver,  is it on the classpath?");
+                e.printStackTrace();
+            } catch (SQLException e) {
+                System.err.print("An SQL exception has occurred (" + e.getMessage() + ")");
+                e.printStackTrace();
+            }
+        } catch (ParseException e) {
+            System.err.print("Unable to parse command line options (" + e.getMessage() + ")");
+        }
+    }
+}
